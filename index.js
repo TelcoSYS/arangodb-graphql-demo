d523e35f64ac2cf35aa30bd08b5ba5484e976e64
gcugli <cgc@cugli.org>
2020-10-16 00:08:18 -0300

    build Schema Language

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 'use strict';
-const schema = require('./schema');
-const gql = require('graphql-sync');
-const graphql = gql.graphql;
-const formatError = gql.formatError;
+/*
+ *  Starting with graphql 0.12 you can also use the official graphql library 
+ *  if you include it in the node_modules folder of your service bundle
+ *  https://www.arangodb.com/docs/stable/foxx-reference-modules-graph-ql.html
+ */
+const graphql = require('graphql');
 const createGraphqlRouter = require('@arangodb/foxx/graphql');
 
-// This is a regular Foxx router.
-const router = createGraphqlRouter({schema, graphiql: true})
+// GraphQL Foxx Router
+const router = createGraphqlRouter({
+	schema: require('./schema'),
+	rootValue: require('./resolvers'),
+	graphiql: true,
+	graphql: graphql
+})
 .summary('GraphQL endpoint')
 .description('GraphQL endpoint for the Star Wars GraphQL example.');
 
 module.context.use(router);
+console.log('GraphQL endpoint enabled')
---
+++ resolvers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+	
+  hero: ({episode}) => ({ id: 'TODO', name: 'not yet implemented' }),
+  human: ({id}) => ({}),
+  droid: ({id}) => ({}),
+  version: () => 'arangodb-graphql-demo v2.0.4'
+
+}
